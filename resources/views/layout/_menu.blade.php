e55c6973a9d4c8d4aeafcb56fa489d941bbb9e15
laurusso <lr.russo@unesp.br>
2023-05-30 16:56:40 -0300

    devs

--- resources/views/layout/_menu.blade.php
+++ resources/views/layout/_menu.blade.php
@@ -43,7 +43,7 @@
 
 <nav class="navbar navbar-expand-lg topo" >
     <div class="container-fluid">
-    <img src="{!! asset('img/logo.png') !!}" class="logomarca">
+    <!-- <img src="{!! asset('img/logo.png') !!}" class="logomarca"> -->
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
--- resources/views/menu/devs.blade.php
+++ resources/views/menu/devs.blade.php
@@ -1,8 +1,39 @@
 <!-- CHAMADA DA DIV FIXED -->
-@include('layout._menu');
+@include('layout._menu')
 
 <div class="body">
-    <h1>CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC</h1>
+    <div class="engloba-texto">
+        <div class="texto-devs">
+            <h1>desenvolvedores</h1>
+            <p>Esses são os sofredores, afinal, ninguém gosta de arduino :(</p>
+        </div>
+        <div class="card-roxo">
+                pessoa laura
+        </div>
+    </div>
+    
+    <div class="engloba-pessoas">
+        <div class="card-azul">
+            pessoa nicole
+        </div>
+        <div class="card-azul">
+            pessoa paula
+        </div>
+        <div class="card-azul">
+            pessoa evellyn
+        </div>
+        <div class="card-amarelo">
+            pessoa pedro
+        </div>
+        <div class="card-amarelo">
+            pessoa eloisa
+        </div>
+        <div class="card-amarelo">
+            pessoa deborah
+        </div>
+    </div>
+    
+    
 </div>
 
 <!-- CHAMADA DA DIV FOOTER -->
